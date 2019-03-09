a411430bdffbdffe74462f63c3746848f22e8565
Ryan Kendrick <rpkendrick@gmail.com>
2019-03-09 00:21:25 +0000

    added build folder for heroku

---
+++ build/precache-manifest.e2cd884cf297ac17e06c1b8cd0ccbe79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "549790b3a50a372a5718c0d4509f1379",
+    "url": "/static/media/app2.549790b3.png"
+  },
+  {
+    "revision": "da58dfb2cbc0acfa97b98417ac547bc7",
+    "url": "/static/media/app1.da58dfb2.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "02050dad5a1df4c58545",
+    "url": "/static/js/main.02050dad.chunk.js"
+  },
+  {
+    "revision": "8ea8c6f381452d1462cd",
+    "url": "/static/js/1.8ea8c6f3.chunk.js"
+  },
+  {
+    "revision": "02050dad5a1df4c58545",
+    "url": "/static/css/main.f4c05cfb.chunk.css"
+  },
+  {
+    "revision": "d17ed51b9d69fb6f68b1307f7f21356c",
+    "url": "/index.html"
+  }
+];
